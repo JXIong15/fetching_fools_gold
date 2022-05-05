70e1c0173665235e3d17dc902732aa7a491db74f
Jou Xiong <j.xiong@fetchrewards.com>
2022-05-05 10:04:56 -0500

    🎉 weighings 🎉  got all gold bars

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
+# maybe add a env file?
 driver.get("http://sdetchallenge.fetchrewards.com/")
 
-def main() :
+def main():
     print(driver.title)
+    assert "React" in driver.title
+    goldBars = driver.find_elements(By.XPATH, "//div[@class='coins']/button")
+
+    for gb in goldBars:
+        print(gb.text)
+    # print(goldBars)
     driver.close()
 
 main()
