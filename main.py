0199d3d23bdaaeb7bd72c4cc5851b38379e0e72d
Jou Xiong <j.xiong@fetchrewards.com>
2022-05-11 11:09:17 -0500

    🎉 main 🎉  changed naming to match python conventions

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ driver.get("http://sdetchallenge.fetchrewards.com/")
 
 
 def main():
-    goldBars = driver.find_elements(By.XPATH, "//div[@class='coins']/button")
-    ans = find_fake(goldBars)
-    results(ans, goldBars)
+    gold_bars = driver.find_elements(By.XPATH, "//div[@class='coins']/button")
+    ans = find_fake(gold_bars)
+    results(ans, len(gold_bars))
     driver.close()
     driver.quit()
 
 
-def find_fake(goldBars):
+def find_fake(gold_bars):
     ans = None
     extra = None
 
@@ -23,58 +23,59 @@ def find_fake(goldBars):
         left = []
         right = []
 
-        if len(goldBars) % 2 != 0:
-            extra = goldBars[len(goldBars)-1]
-            goldBars.pop()
+        if len(gold_bars) % 2 != 0:
+            extra = gold_bars[len(gold_bars)-1]
+            gold_bars.pop()
 
         # make sure board is empty before every weighing
         driver.find_element(By.XPATH, "//button[contains(text(),'Reset')]").click()
-        weigh_ins(goldBars, left, right)
+        weigh_ins(gold_bars, left, right)
         time.sleep(1)   # wait for weigh-in result to process
-        goldBars, ans = compare(goldBars, left, right, extra)
+        gold_bars, ans = compare(gold_bars, left, right, extra)
 
     return ans
 
 
  # split bars in two equal piles for weighing
-def weigh_ins(goldBars, left, right):
-    halfway = int(len(goldBars) / 2)
-    for i in range(0, len(goldBars)):
+def weigh_ins(gold_bars, left, right):
+    halfway = int(len(gold_bars) / 2)
+    for i in range(0, len(gold_bars)):
         id = ""
         if i < halfway:
-            left.append(goldBars[i])
+            left.append(gold_bars[i])
             id = f"left_{i}"
         else:
-            right.append(goldBars[i])
+            right.append(gold_bars[i])
             id = f"right_{i}"
                 
         # input values to weigh
-        driver.find_element(By.ID, id).send_keys(goldBars[i].text)
+        driver.find_element(By.ID, id).send_keys(gold_bars[i].text)
 
     driver.find_element(By.ID, "weigh").click()
 
 
 # use weigh-in results to determine what to do next
-def compare(goldBars, left, right, extra):
+def compare(gold_bars, left, right, extra):
     ans = None
     res = driver.find_element(By.XPATH, "//div[@class='result']/button")
+
     # compare piles
     if res.text == "=":
         ans = extra
     elif res.text == "<":
-        goldBars = left
+        gold_bars = left
         if len(left) == 1:
             ans = left[0]
     elif res.text == ">":
-        goldBars = right
+        gold_bars = right
         if len(right) == 1:
             ans = right[0]
 
-    return goldBars, ans
+    return gold_bars, ans
 
 
 # prints results to console
-def results(ans, goldBars):
+def results(ans, n):
     print("Fake Gold Bar: ", f"#{ans.text}")
 
     weighings = driver.find_elements(By.XPATH, "//div[@class='game-info']/ol/li")
@@ -84,7 +85,7 @@ def results(ans, goldBars):
         w.append(weighing.text)
     print("Weighing Result(s): ", w)
     print("Number of Weighing(s): ", len(w))
-    assert len(w) <= min_weighings(len(goldBars))
+    assert len(w) <= min_weighings(n)
 
     driver.find_element(By.ID, f"coin_{ans.text}").click()
     alert = driver.switch_to.alert
